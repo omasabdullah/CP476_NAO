f234761989e90c98fbceeb396169b3b003a1dac1
Omas Abdullah <omas.abdullah@gmail.com>
2014-02-06 16:49:20 -0500

    Removed Unecessary files
    Start merging modules into one file

--- src/wlu/cp476/MoveHead.java
+++ src/wlu/cp476/MoveHead.java
@@ -9,13 +9,14 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.Scanner;
 
+import com.aldebaran.proxy.ALMemoryProxy;
 import com.aldebaran.proxy.ALMotionProxy;
 import com.aldebaran.proxy.ALTextToSpeechProxy;
 import com.aldebaran.proxy.ALVideoDeviceProxy;
 import com.aldebaran.proxy.Variant;
 
 public class MoveHead {
-	private static String NAOQI_IP = "192.168.1.118";
+	private static String NAOQI_IP = "192.168.1.122";
 	private static int NAOQI_PORT = 9559;
 	private BufferedImage img;
 
@@ -29,6 +30,11 @@ public class MoveHead {
 		ALMotionProxy motion = new ALMotionProxy(NAOQI_IP, NAOQI_PORT);
 		ALTextToSpeechProxy tts = new ALTextToSpeechProxy(NAOQI_IP, NAOQI_PORT);
 		ALVideoDeviceProxy videoDevice = new ALVideoDeviceProxy(NAOQI_IP, NAOQI_PORT);
+		ALMemoryProxy memory = new ALMemoryProxy(NAOQI_IP, NAOQI_PORT);
+		
+		memory.insertData("answer", 42);
+		Variant res = memory.getData("answer");
+		System.out.format("The answer is %d\n", res.toInt());
 
 		videoDevice.subscribe("java", 1, 11, 250);
 		Variant ret = videoDevice.getImageRemote("java");
@@ -47,7 +53,8 @@ public class MoveHead {
 			System.out.println("Error");
 		else
 			System.out.println("Found Nao");
-			
+		
+		//HEAD TURNING
 		// This lets you use bound methods that excpects ALValue from Java:
 		Variant names  = new Variant(new String[] {"HeadYaw" });
 		Variant angles = new Variant(new float[] { -0.5f, 0.5f, 0.0f });
@@ -56,6 +63,12 @@ public class MoveHead {
 		motion.setStiffnesses(new Variant(new String[] {"HeadYaw"}), new Variant(new float[] {1.0f}));
 		motion.angleInterpolation(names, angles, times, true);
 		motion.setStiffnesses(new Variant(new String[] {"HeadYaw"}), new Variant(new float[] {0.0f}));
+		
+		//MOVEMENT
+		motion.wakeUp();
+		//motion.moveInit();
+		//motion.moveTo(0.0f, 0.0f, 0.0f);
+		motion.rest();
 		
 		// Scan in input from user
     	Scanner scanner = new Scanner(System.in);
